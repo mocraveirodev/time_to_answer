75a47737a1d0dacfc41bb0f3968f53f1e40f1633
Monica Craveiro <craveiromonica@gmail.com>
2024-10-21 01:24:03 -0300

    Implementando template view no User

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,7 +13,10 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 
 # app/assets
-Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css )
+Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css users_backoffice.js users_backoffice.css )
 
 # lib/assets
-Rails.application.config.assets.precompile += %w( sb-admin-2.min.js sb-admin-2.min.css )
+Rails.application.config.assets.precompile += %w( sb-admin-2.min.js sb-admin-2.min.css gentelella.min.js gentelella.min.css img.jpg )
+
+# vendor/assets
+Rails.application.config.assets.precompile += %w( jquery-2.2.3/dist/jquery.js )
